899bf9ab6249760da85a985f4e6754e32a0996a8
Sourabh Kumar <sourabhkumar765551@gmail.com>
2025-11-24 02:42:58 +0530

    Enable serverActions experimental flag

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactStrictMode: true,
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
